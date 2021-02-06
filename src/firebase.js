750284b2b51fa2462e4c2c5475ee77bea0399d6e
a0g06il <ayush.goyal@walmart.com>
2021-02-06 13:50:09 +0530

    final build

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,5 @@
 import firebase from 'firebase';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBw6TzGplsOMCEU9r_z9P2OZqLlbcWT0-0",
-  authDomain: "snapchat-f1b2e.firebaseapp.com",
-  projectId: "snapchat-f1b2e",
-  storageBucket: "snapchat-f1b2e.appspot.com",
-  messagingSenderId: "807293513478",
-  appId: "1:807293513478:web:43770798f39300212ef887"
-};
+import firebaseConfig from './config';
 
 const firebaseapp = firebase.initializeApp(firebaseConfig);
 
